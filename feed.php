f3ab39b78bd2dd26d858c927f3487f4a32cc48a6
Alexander Berezovsky <a.b@usa.com>
2014-01-13 14:43:57 -0800

    fix for url pointed to the comments

--- feed.php
+++ feed.php
@@ -10,11 +10,18 @@ $stories = json_decode($stories);
 
 if(is_object($stories) && isset($stories->items) && is_array($stories->items)) {
     foreach($stories->items as $story) {
-        $workflow->result(time()."-HN-".$story->id, $story->url, $story->title, "Posted ".$story->postedAgo." by ".$story->postedBy.". ".$story->commentCount." comments. ".$story->points." points.", $icon_path);
+      if(preg_match('/^\/comments\/(\d+)/', $story->url, $match)) {
+        $url = "https://news.ycombinator.com/item?id=$match[1]";
+      } else {
+        $url = $story->url;
+      }
+
+      $workflow->result(time()."-hn-".$story->id, $url, $story->title, "posted ".$story->postedago." by ".$story->postedby.". ".$story->commentcount." comments. ".$story->points." points.", $icon_path);
     }
-}
-else {
-    $workflow->result(time()."-HN-0", "https://news.ycombinator.com/", "Unavailable", "Damn bro! The Hacker News feed is broke. Go to the front page?", $icon_path);
+} else {
+    $workflow->result(time()."-hn-0", "https://news.ycombinator.com/", "unavailable", "damn bro! the hacker news feed is broke. go to the front page?", $icon_path);
 }
 
 echo $workflow->toxml();
+
+?>
